119b176dc4b7d32ea6b94719db9dcd229941c144
Plain Jane <jane99hsu@gmail.com>
2020-09-06 15:39:42 -0700

    updated request logic with dynamic variable

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,17 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+
+        // this line logs the array of repo data that we are targeting.  See 6.1.5 // 
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+
+    console.log("outside");
 };
 
-getUserRepos();
+getUserRepos("plainjane99");
